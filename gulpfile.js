d1da5c02e365287843376d8e19ddd80ea73a7f7d
Kevin <Kwordener1@gmail.com>
2020-08-20 16:11:54 -0500

    css clean up pre conversion done, responsiveness complete on screen sizes: 325, 768, 1024, 1600, and 2560.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('sass', function(){
-  return gulp.src('scss/main.scss')
-    .pipe(sass().on('error', sass.logError)) // Using gulp-sass
-    .pipe(gulp.dest('css'))
-});
-
-gulp.task('watch', function(){
-  gulp.watch('scss/**/*.scss'); 
-  // Other watchers
-})
-
-gulp.task('default', gulp.series(['watch', 'sass']));
+const { dest, src, watch, series } = require('gulp');
+let sass = require('gulp-sass');
+var paths = {
+  srcSCSS: 'scss/**/*.scss',
+  srcMainSCSS: 'scss/main.scss',
+  distCSS: 'css',
+};
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded',
+};
+function compile(cb) {
+  src(paths.srcMainSCSS)
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(dest(paths.distCSS));
+  cb();
+}
+function watchFiles(cb) {
+  watch(paths.srcSCSS, compile);
+  cb();
+}
+exports.compile = compile;
+exports.default = series(watchFiles);
